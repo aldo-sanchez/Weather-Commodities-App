ea75367dba7509fba37acc331127f46ee014f240
alejandroharb <alejandroharb@gmail.com>
2016-12-07 17:16:51 -0600

    push temp data into temporary array

--- assets/javascript/weather-api.js
+++ assets/javascript/weather-api.js
@@ -7,16 +7,17 @@
 //token for accessing API
 var token = "sUtCbQRELKcKTvcahYjUDGMtwcoeqrmz";
 // ajax call for city location available
-$.ajax({
-	url:"https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=FIPS:17&datacategoryid=ZIP&sortfield=ZIP&sortorder=asc&limit=50",
-	headers:{token:token}
-	}).done(function(response){
-		console.log("city locations:")
-		console.log(response)
-});
+// $.ajax({
+// 	url:"https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=ST&sortfield=id&sortorder=asc&limit=50",
+// 	headers:{token:token}
+// 	}).done(function(response){
+// 		console.log("city locations:")
+// 		console.log(response)
+// });
 
-
+var tempAvg = [];
 //data set: TAVG(Avg temp), TPCP(total precipitation), GSOY(global summary of the year), GSOM(global summ of month), PRECIP_15(Precipitation 15 Minute), DP10(Num of days with greater than or eq to 1 inch of precipitation)
+
 var dataSet = "GSOM";
 //category id's: TEMP, PRCP(precipitation), WATER, SUTEMP(summer temperature), SUPRCP(summer preciptation)
 var dataCategory = "TEMP";
@@ -29,9 +30,26 @@ var station = "GHCND:USC00110072";
 //start date
 var startDate = "2000-01-01";
 //end date
-var endDate = "2000-12-31";
+var endDate = "2009-12-31";
 //AJAX query
-var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid="+dataSet+"&datatypeid="+dataType+"&stationid="+station+"&units=metric&startdate="+startDate+"&enddate="+endDate+"&limit=100"
+var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid="+dataSet+"&datatypeid="+dataType+"&stationid="+station+"&units=metric&startdate="+startDate+"&enddate="+endDate+"&limit=10"
 $.ajax({ url:queryURL, headers:{ token:token } }).done(function(response){
 	console.log(response);
+    var data = response.results;
+    console.log(data);
+    //looping through ajax JSON to store relevant data (date, temp) in array
+    for(var i in data){
+        if(data.hasOwnProperty(i)) {
+            var date = data[i].date;
+            var temp = data[i].value;
+            var tempDateObj = {
+                "date":date,
+                "temp":temp
+            }
+            tempAvg.push(tempDateObj)
+        }
+    }
+    console.log("Avg temp Array: " + tempAvg);
 })
+
+
