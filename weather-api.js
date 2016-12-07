7190a37bf5511818dd03cea1e12b76de87d81c59
alejandro harb <alejandro harb>
2016-12-07 14:14:31 -0600

    index file for weather-api testing

--- weather-api.js
+++ weather-api.js
@@ -1,14 +1,14 @@
 
 //AJAX query to get list of data types from 1000 to 1527
-var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes?limit=1000&offset=1000"
-$.ajax({ url:queryURL, headers:{ token:token } }).done(function(response){
-	console.log(response);
-})
+// var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes?limit=1000&offset=1000"
+// $.ajax({ url:queryURL, headers:{ token:token } }).done(function(response){
+// 	console.log(response);
+// })
 //token for accessing API
 var token = "sUtCbQRELKcKTvcahYjUDGMtwcoeqrmz";
-//ajax call for city location available
+// ajax call for city location available
 $.ajax({
-	url:"https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CITY&sortfield=name&sortorder=desc&limit=1000",
+	url:"https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=FIPS:17&datacategoryid=ZIP&sortfield=ZIP&sortorder=asc&limit=50",
 	headers:{token:token}
 	}).done(function(response){
 		console.log("city locations:")
@@ -17,19 +17,21 @@ $.ajax({
 
 
 //data set: TAVG(Avg temp), TPCP(total precipitation), GSOY(global summary of the year), GSOM(global summ of month), PRECIP_15(Precipitation 15 Minute), DP10(Num of days with greater than or eq to 1 inch of precipitation)
-var dataSetId = "GSOM";
+var dataSet = "GSOM";
 //category id's: TEMP, PRCP(precipitation), WATER, SUTEMP(summer temperature), SUPRCP(summer preciptation)
-var dataCategoryId = "TEMP";
+var dataCategory = "TEMP";
 //data type
-var dataTypeId = "TAVG";
+var dataType = "TAVG";
 //location
-var locationId = "28801";
+var location = "FIPS:17";
+//station
+var station = "GHCND:USC00110072";
 //start date
-var startDate = "2010-05-01";
+var startDate = "2000-01-01";
 //end date
-var endDate = "2010-12-31";
+var endDate = "2000-12-31";
 //AJAX query
-var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid="+dataSetId+"&datatypeid="+dataTypeId+"&locationid=ZIP:"+locationId+"&units=metric&startdate="+startDate+"&enddate="+endDate+"&limit=20"
+var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid="+dataSet+"&datatypeid="+dataType+"&stationid="+station+"&units=metric&startdate="+startDate+"&enddate="+endDate+"&limit=100"
 $.ajax({ url:queryURL, headers:{ token:token } }).done(function(response){
 	console.log(response);
 })
