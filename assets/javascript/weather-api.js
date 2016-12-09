6ba837a0e435baa7713eef19d49f3d84ccd45e02
alejandroharb <alejandroharb@gmail.com>
2016-12-08 19:45:00 -0600

    added for-in loop for api json to create cleaned up object

--- assets/javascript/weather-api.js
+++ assets/javascript/weather-api.js
@@ -1,22 +1,15 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyD5onGD-yX2IvrbzyEiNJ2Y7DfsjNQz0EA",
+authDomain: "weather-commodities-app.firebaseapp.com",
+databaseURL: "https://weather-commodities-app.firebaseio.com",
+storageBucket: "weather-commodities-app.appspot.com",
+messagingSenderId: "917117502463"
+};
+firebase.initializeApp(config);
 
-//AJAX query to get list of data types from 1000 to 1527
-// var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes?limit=1000&offset=1000"
-// $.ajax({ url:queryURL, headers:{ token:token } }).done(function(response){
-// 	console.log(response);
-// })
 //token for accessing API
 var token = "sUtCbQRELKcKTvcahYjUDGMtwcoeqrmz";
-// ajax call for city location available
-// $.ajax({
-// 	url:"https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOM&datatypeid=TMAX&datatypeid=TAVG&locationid=CITY:AE000001&units=metric&startdate=2010-05-01&enddate=2010-12-31",
-// 	headers:{token:token}
-// 	}).done(function(response){
-// 		console.log("city locations:")
-// 		console.log(response)
-// });
-
-var tempAvg = [];
-//data set: TAVG(Avg temp), TPCP(total precipitation), GSOY(global summary of the year), GSOM(global summ of month), PRECIP_15(Precipitation 15 Minute), DP10(Num of days with greater than or eq to 1 inch of precipitation)
 
 var dataSet = "GSOM";
 //category id's: TEMP, PRCP(precipitation), WATER, SUTEMP(summer temperature), SUPRCP(summer preciptation)
@@ -32,28 +25,40 @@ var loc = "CITY:US170002";
 //station
 var station = "GHCND:USC00110072";
 //start date
-var startDate = "2010-05-01";
+var startDate = "2005-05-01";
 //end date
 var endDate = "2010-12-31";
 //AJAX query
-var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid="+dataSet+"&datatypeid="+dataType1+"&datatypeid="+dataType2+"&datatypeid="+dataType3+"&locationid="+loc+"&units=metric&startdate=2010-05-01&enddate=2010-12-31&limit=1000";
+var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid="+dataSet+"&datatypeid="+dataType2+"&locationid="+loc+"&units=metric&startdate=2005-01-01&enddate=2010-12-31&limit=10";
 $.ajax({ url:queryURL, headers:{ token:token } }).done(function(response){
 	// console.log(response);
     var data = response.results;
+    //dynamically created object of cleaned data for location, date, temp
+    var weatherObj = {};
     console.log(data);
+    //function to fetch JSON date, temp, and location and creates new property on this object
+    function weatherMetaData(location, date, temp) {
+        this.location = location;
+        this.date = date;
+        this.temp = temp;
+    }
     //looping through ajax JSON to store relevant data (date, temp) in array
     for(var i in data){
+        //checks if property index has value
         if(data.hasOwnProperty(i)) {
-            var date = data[i].date;
-            var temp = data[i].value;
-            var tempDateObj = {
-                "date":date,
-                "temp":temp
-            }
-            tempAvg.push(tempDateObj)
+            //variable holds date data for i object
+            var dateData = data[i].date;
+            // variable holds temperature data for i object
+            var tempData = data[i].value;
+            //creates location, date, and temp property on each property index of weather Obj.
+            weatherObj[i] = new weatherMetaData(loc, dateData, tempData);
         }
     }
-    // console.log("Avg temp Array: " + tempAvg);
+    console.log("constructed object: ");
+    console.log(weatherObj);
 })
-
-
+  var database = firebase.database();
+  var weatherData = database.ref();
+  function storeData() {
+    weatherData.push()
+  }
