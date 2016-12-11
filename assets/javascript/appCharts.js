858b6e7dfcc5daafbb172594d40852e042616e4d
Aldo Sanchez <Aldo Sanchez>
2016-12-11 13:31:56 -0600

    :construction: Finding best look for weather plots

--- assets/javascript/appCharts.js
+++ assets/javascript/appCharts.js
@@ -1,10 +1,40 @@
 console.log("hello world");
 
+// var data = {
+//   labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+//   series: [
+//     [5,2,4,2,0]
+//   ]
+// };
+
+// new Chartist.Line('.ct-chart', data);
+
+// var oneDArray = [1,2,3,4,5];
+// var twoDArray = [[1,2,3,4,5],["jun","may","aug","july","sept"]];
+
+labelArray = [];
+seriesArray = [];
+
+var tempData = [
+    ['2005-01-01','2005-02-01','2005-03-01','2005-04-01','2005-05-01','2005-06-01'],
+    [32, 45, 56, 44, 67, 80]];
+
+
 var data = {
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+  labels: tempData[0],
   series: [
-    [5,2,4,2,0]
-  ]
+    tempData[1]
+  ],
+  
 };
 
-new Chartist.Line('.ct-chart', data);
+var options = {
+  low: 0,
+  showArea: true,
+  showPoint: false,
+  fullWidth: true,
+  width: '200px',
+  height: '200px'
+};
+
+new Chartist.Line('.ct-chart', data, options);
