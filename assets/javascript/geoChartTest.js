c3c5c7e9262d46edf028467db26856cae0cf831b
Aldo Sanchez <Aldo Sanchez>
2016-12-12 21:00:08 -0600

    :construction: Added event listener for map chart

--- assets/javascript/geoChartTest.js
+++ assets/javascript/geoChartTest.js
@@ -1,59 +1,53 @@
-// console.log('map Vis')
-// function drawVisualization() {
-//   var data = google.visualization.arrayToDataTable([
-//     ['State', 'Foo Factor'],
-//     ['US-TX', 200],
-//     ['US-IN', 300],
-//     ['US-IA', 20],
-//     ['US-RI', 150]
-//   ]);
+console.log('map Vis')
 
-//   var geochart = new google.visualization.GeoChart(
-//       document.getElementById('visualization'));
-// var options = {region: "US", resolution: "provinces"};
 
-//   geochart.draw(data, options);
-// }
 
-// google.charts.setOnLoadCallback(drawVisualization)
+// google.charts.load('upcoming', {'packages':['geochart']});
+google.charts.setOnLoadCallback(drawMap);
 
-// Set a callback to run when the Google Visualization API is loaded.
- google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1], 
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
-
-        // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
- 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('visualization'));
-
-        function selectHandler() {
-          var selectedItem = chart.getSelection()[0];
-          if (selectedItem) {
-            var topping = data.getValue(selectedItem.row, 0);
-            alert('The user selected ' + topping);
-          }
+function drawMap() {
+    var data = google.visualization.arrayToDataTable([
+        ['State', 'Foo Factor'],
+    ['US-IL', 200],
+    ['US-IN', 300],
+    ['US-IA', 20],
+    ['US-RI', 150],
+    ['US-TX',400]
+    ]);
+    
+    var options = {
+      width: 556, 
+      height: 347, 
+      region: "US", 
+      resolution: "provinces"
+        // dataMode: 'regions',
+        // width: 834,
+        // height: 521
+    }; 
+    
+    var container = document.getElementById('mapVis');
+    var chart = new google.visualization.GeoChart(container);
+    
+    function myClickHandler(){
+        var selection = chart.getSelection();
+        var message = '';
+        for (var i = 0; i < selection.length; i++) {
+            var item = selection[i];
+            if (item.row != null && item.column != null) {
+                message += '{row:' + item.row + ',column:' + item.column + '}';
+            } else if (item.row != null) {
+                message += '{row:' + item.row + '}';
+            } else if (item.column != null) {
+                message += '{column:' + item.column + '}';
+            }
         }
-
-        google.visualization.events.addListener(chart, 'select', selectHandler);    
-        chart.draw(data, options);
-      }
+        if (message == '') {
+            message = 'nothing';
+        }
+        alert('You selected ' + message);
+    }
+    
+    google.visualization.events.addListener(chart, 'select', myClickHandler);
+    
+    chart.draw(data, options);
+}
