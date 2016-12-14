ac69b3ac5a75f85bc26f1f45981d79f93bd56a3a
Aldo Sanchez <Aldo Sanchez>
2016-12-13 19:56:00 -0600

    :construction: Added chart.js library and chart

--- assets/javascript/appCharts.js
+++ assets/javascript/appCharts.js
@@ -5,7 +5,7 @@ var tempData = [
     ['2005-01-01','2005-02-01','2005-03-01','2005-04-01','2005-05-01','2005-06-01'],
     [32, 45, 56, 44, 67, 80]];
 
-var precData = [
+var priceData = [
 ['2005-01-01','2005-02-01','2005-03-01','2005-04-01','2005-05-01','2005-06-01'],
 [1, 5, 2, 3, 3, 10]];
 
@@ -31,10 +31,10 @@ function drawChartDual(){
 var data = new google.visualization.DataTable();
       data.addColumn('string', 'Month');
       data.addColumn('number', "temperature");
-      data.addColumn('number', "precipitation");
+      data.addColumn('number', "price");
 
       for (i = 0; i < tempData[0].length; i++) {
-        chartRows[i] = [tempData[0][i], tempData[1][i], precData[1][i]];
+        chartRows[i] = [tempData[0][i], tempData[1][i], priceData[1][i]];
     };
 
       data.addRows(chartRows)
@@ -55,7 +55,7 @@ var data = new google.visualization.DataTable();
 
       var materialOptions = {
         chart: {
-          title: 'temperature and precipitation'
+          title: 'temperature and price'
         },
         width: 900,
         height: 500,
@@ -64,7 +64,7 @@ var data = new google.visualization.DataTable();
           0: {axis: 'temperature',
               color: '#43459d'
             },
-          1: {axis: 'precipitation',
+          1: {axis: 'price',
               color: '#e7711b'
             }
         },
@@ -72,7 +72,7 @@ var data = new google.visualization.DataTable();
           // Adds labels to each axis; they don't have to match the axis names.
           y: {
             temperature: {label: 'Temps (Celsius)'},
-            precipitation: {label: 'precipitation'}
+            price: {label: 'price ($)'}
           },
           gridlines:{
             color: '#0061ff'
@@ -90,3 +90,66 @@ var data = new google.visualization.DataTable();
 
 // google.charts.setOnLoadCallback(drawChart);
 google.charts.setOnLoadCallback(drawChartDual);
+
+var ctx = document.getElementById("chartjs").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    "labels": [
+      "01.12.2015",
+      "02.12.2015",
+      "03.12.2015",
+      "04.12.2015",
+      "30.12.2015"
+    ],
+    "datasets": [{
+      "label": "DEA Burrweiler test Abgabe",
+      yAxisID: "y-axis-0",
+      "data": [
+        8.7913,
+        8.6985,
+        8.7914,
+        8.7948,
+        8.7882
+      ]
+    }, {
+      "label": "DEA Burrweiler Druck Zulauf",
+      "fill": "false",
+      yAxisID: "y-axis-1",
+      "data": [
+        4.5997,
+        4.5526,
+        4.5915,
+        4.5937,
+        4.5795
+      ],
+      backgroundColor: "rgba(000,111,111,.5)",
+      fill: false
+    }, {
+      "label": "DMS Flemlingen Durchfluss",
+      "fill": "false",
+      yAxisID: "y-axis-1",
+      "data": [
+        1.9588,
+        2.4226,
+        2.1392,
+        2.223,
+        1.9048
+      ],
+      backgroundColor: "blue",
+      fill: false
+    }]
+  },
+  options: {
+  
+    scales: {
+      yAxes: [{
+        position: "left",
+        "id": "y-axis-0",
+      }, {
+        position: "right",
+        "id": "y-axis-1",
+      }]
+    }
+  }
+});
