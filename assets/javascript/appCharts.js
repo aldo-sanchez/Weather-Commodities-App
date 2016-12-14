c76044b1a254f2f22b65e00748304f246c06698a
Aldo Sanchez <Aldo Sanchez>
2016-12-13 22:39:44 -0600

    :rotating_light: fixed conflicts

--- assets/javascript/appCharts.js
+++ assets/javascript/appCharts.js
@@ -1,6 +1,7 @@
 console.log("hello world");
-
+// variables: these will be pulled from alex and fred. for now these are placeholders.
 var dates = ['2005-01-01','2005-02-01','2005-03-01','2005-04-01','2005-05-01','2005-06-01']
+
 var tempData = [
     ['2005-01-01','2005-02-01','2005-03-01','2005-04-01','2005-05-01','2005-06-01'],
     [32, 45, 56, 44, 67, 80]];
@@ -13,15 +14,14 @@ var priceData = [
 ['2005-01-01','2005-02-01','2005-03-01','2005-04-01','2005-05-01','2005-06-01'],
 [133, 137, 145, 152, 160, 130]];
 
+// Combine my data for charting.  This may change based on how data is received.
 totalData = [precipData[1],priceData[1],tempData[1]];
 var chartDataArray = [];
 var axesArray = [];
 var labelArray = ['precipitation','price','temperature'];
 var positionArray = ['left','right','left'];
 
-
 var ctx = document.getElementById("chartjs").getContext("2d");
-
 for (i = 0; i < 3; i++){
   chartDataArray[i] = {
     "label": labelArray[i],
@@ -50,3 +50,6 @@ var myChart = new Chart(ctx, {
     }
   }
 });
+
+
+
