ac43e2d05db719022a09d0c41853d8f3ff5c4f80
alejandroharb <alejandroharb@gmail.com>
2016-12-28 15:55:25 -0600

    finished weather api function, styled weather data

--- assets/javascript/weatherForecast.js
+++ assets/javascript/weatherForecast.js
@@ -7,6 +7,23 @@ function weatherForecastApi() {
         method: 'GET'
     }).done(function(response){
         console.log(response)
+        var data = response.forecast.simpleforecast.forecastday;
+        for(var i = 0; i < data.length; i++){
+            var day = data[i].date.weekday;
+            var conditionsTxt = data[i].conditions;
+            var tempHigh = data[i].high.fahrenheit;
+            var tempLow = data[i].low.fahrenheit;
+            var precipitationProb = data[i].pop;
+            var imgURL = data[i].icon_url;
+            var newLi = $('<li>');
+            var imgElement = ('<img class = "responsive-img" src="'+imgURL+'">');
+            var dayTitle = $('<h3>'+day+'</h3><h4>'+conditionsTxt+'</h4>')
+            var weatherInfo = ('<h5>Temperature (F)</h5> '+tempHigh+'/'+tempLow+'<h5>Chance of Rain (%)</h5>'+precipitationProb);
+            var newChildLi = $('#weatherForecast').append(newLi);
+            newLi.append(dayTitle);
+            newLi.append(imgElement);
+            newLi.append(weatherInfo);
+        }
     });
 };
 
